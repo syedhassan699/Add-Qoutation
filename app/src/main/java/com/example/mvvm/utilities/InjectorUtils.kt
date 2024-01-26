b108c80a33996cdc64ffb26772702cdc37a2ed87
hassan123 <151631976+syedhassan699@users.noreply.github.com>
2024-01-26 18:16:05 +0500

    Committed

--- app/src/main/java/com/example/mvvm/utilities/InjectorUtils.kt
+++ app/src/main/java/com/example/mvvm/utilities/InjectorUtils.kt
@@ -1,11 +1,13 @@
 package com.example.mvvm.utilities
 
+import android.annotation.SuppressLint
 import com.example.mvvm.data.FakeDatabase
 import com.example.mvvm.data.QuoteRepository
 import com.example.mvvm.ui.QuoteViewModelFactory
 
 object InjectorUtils {
 
+    @SuppressLint("SuspiciousIndentation")
     fun provideQuoteViewModelFactory(): QuoteViewModelFactory{
     val  quoteRepository = QuoteRepository.getInstance(FakeDatabase.getInstance().quoteDao)
         return QuoteViewModelFactory(quoteRepository)
