cff71d7cc5c7e77939fd5246ab05ad9496acf3dd
hassan123 <151631976+syedhassan699@users.noreply.github.com>
2024-01-26 18:30:41 +0500

    Completed

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.mvvm.data.Quote
@@ -30,10 +31,16 @@ class MainActivity : AppCompatActivity() {
             binding?.textViewQuotes?.text = stringBuilder.toString()
         })
         binding?.buttonAddQuote?.setOnClickListener{
-            val quote = Quote(binding?.editTextQuote?.text.toString(),binding?.editTextAuthor?.text.toString())
-            viewModel.addQuote(quote)
-            binding?.editTextQuote?.setText("")
-            binding?.editTextAuthor?.setText("")
+            if (binding?.editTextQuote?.text!!.isNotEmpty() && binding?.editTextAuthor?.text!!.isNotEmpty()){
+                val quote = Quote(binding?.editTextQuote?.text.toString(),binding?.editTextAuthor?.text.toString())
+                viewModel.addQuote(quote)
+                binding?.editTextQuote?.setText("")
+                binding?.editTextAuthor?.setText("")
+            }else if (binding?.editTextQuote?.text!!.isEmpty()){
+                Toast.makeText(this, "Please Enter Quotation", Toast.LENGTH_SHORT).show()
+            }else if (binding?.editTextAuthor?.text!!.isEmpty()){
+                Toast.makeText(this, "Please Enter Author Name", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
